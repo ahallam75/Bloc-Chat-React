886b13d6a8ef3e76d486fcad83b93d512823d354
ahallam <alexhallam75@gmail.com>
2018-06-25 12:22:31 -0400

    RoomList Component: ComponentDidMount.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
@@ -21,7 +22,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bloc Chat</h1>
+          <h1 className="App-title">Bloc Chat with React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 
-/*export class RoomList extends React.Component {
+export class RoomList extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      rooms: ['Room 1', 'Room 2', 'Room 3'] //Added per checkpoint 2 instructions.
+      rooms: [] //Added per checkpoint 2 instructions.
     };
-    this.roomsRef = this.props.firebase.database().ref('rooms'); */
-    
-    
-//the <p> tag below is just to test to see if the component is rendering.
-
-class RoomList extends React.Component {
-  render() {
-    return <p>This is just a test</p>;
+    this.roomsRef = this.props.firebase.database().ref('rooms'); 
   }
 }
 
-ReactDOM.render(<RoomList />, getElementById('app'));    
-  }
+componentDidMount() {
+  this.roomsRef.on('child_added', snapshot => {
+    console.log(snapshot);
+  });
+}
 
+/* 
 
-
- /*componentDidMount() {
+componentDidMount() {
       this.roomsRef.on('child_added', snapshot => {
         const room = snapshot.val();
         room.key = snapshot.key;
-        this.setState({ rooms: this.state.rooms.concat( room ) })
+        this.setState( { rooms: this.state.rooms.concat( room ) } )
       });
     }
 
+*/
+
+/*
 render () {
   return (
     {this.state.rooms.map()}  
   )
 }
 */
+
