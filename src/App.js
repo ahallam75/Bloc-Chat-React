838879f7ce6f4c9947113659570227176dd691bb
ahallam <alexhallam75@gmail.com>
2018-06-27 10:25:05 -0400

    Removed boilerplate code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
 import { RoomList } from './components/RoomList';
@@ -20,13 +18,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bloc Chat with React</h1>
+        <header>
+          <h1>Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <RoomList firebase={firebase} />
       </div>
     );
